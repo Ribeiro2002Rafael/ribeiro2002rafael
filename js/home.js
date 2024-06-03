036ee73edeb0f6e6ba9a6b8da250696c85c32551
Ribeiro2002Rafael <161616406+Ribeiro2002Rafael@users.noreply.github.com>
2024-06-03 13:09:27 +0100

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,5 @@
+if (window.innerWidth <= 600) {
+    // Executar código específico para telas menores
+    document.getElementById('mobile-menu').style.display = 'block';
+    document.getElementById('desktop-menu').style.display = 'none';
+}
