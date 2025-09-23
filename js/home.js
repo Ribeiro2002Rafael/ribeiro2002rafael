22d4847549d430dbeb87dd21cdffbe31db07c650
Ribeiro2002Rafael <161616406+Ribeiro2002Rafael@users.noreply.github.com>
2025-09-23 19:59:24 +0100

    Delete js directory

--- js/home.js
+++
@@ -1,5 +0,0 @@
-if (window.innerWidth <= 600) {
-    // Executar código específico para telas menores
-    document.getElementById('mobile-menu').style.display = 'block';
-    document.getElementById('desktop-menu').style.display = 'none';
-}
